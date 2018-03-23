27ba08fd3740c2219c67bb6602a8e3e80f7cc8cc
annaboyatyuk <annaboyatyuk@gmail.com>
2018-03-22 21:03:22 -0700

    readme one at a time

--- anna-eric/scripts/articleView.js
+++ anna-eric/scripts/articleView.js
@@ -43,7 +43,7 @@ articleView.handleAuthorFilter = function() {
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
       $articleShow.hide();
       let $author = $(this).val();
-      $(`article[data-author=${$author}]`).show();
+      $(`article[data-author=${$author}]`).fadeIn();
 
     } else {
       // TODONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
@@ -66,7 +66,7 @@ articleView.handleCategoryFilter = function() {
     if ($(this).val()) {
       $articleShow.hide();
       let $category = $(this).val();
-      $(`article[data-category=${$category}]`).show();
+      $(`article[data-category=${$category}]`).fadeIn();
 
     } else {
       $articleShow.show();
@@ -82,7 +82,8 @@ articleView.handleMainNav = function() {
   // TODONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  $('.main-nav .tab').on('click', function() {
+  $('.main-nav .tab').on('click', function(event) {
+    event.preventDefault();
     let $tab = $(this).data('content');
     $('.tab-content').hide();
     $(`#${$tab}`).fadeIn();
@@ -97,12 +98,10 @@ articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide();
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
-  $('article .read-on').on('click', function(event) {
+  $('article').on('click', '.read-on', function(event) {
     event.preventDefault();
-    // let $readOn = $(this).data('author');
-    // $(`article[data-category=${$readOn}]`).show();
-    $('.read-on').hide();
-    $('.article-body *').show();
+    $(this).parent().find('.article-body *').fadeIn();
+    $(this).hide();
   });
 }
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
